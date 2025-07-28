9cd7f8ec969a8c9ae40c53db9e18200960a422f5
Mustafa Ozturk <68792671+mustafa-ozturk@users.noreply.github.com>
2025-07-28 13:53:39 -0400

    middleware

---
+++ src/api/middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+export const middlewareLogResponses = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.on("finish", () => {
+    const statusCode = res.statusCode;
+
+    if (statusCode >= 300) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+    }
+  });
+
+  next();
+};
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { NextFunction, Request, Response } from "express";
+import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middleware.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(middlewareLogResponses);
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
