480b06eed050b6c1a489ab860af952aaea4e1950
Mustafa Ozturk <68792671+mustafa-ozturk@users.noreply.github.com>
2025-07-30 12:04:58 -0400

    auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,9 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
+import { UnAuthorizedError } from "./error";
+
+const TOKEN_ISSUER = "chirpy";
 
 export const hashPassword = async (password: string): Promise<string> => {
   return await bcrypt.hash(password, 1);
@@ -9,4 +14,45 @@ export const checkPasswordHash = async (
   hash: string
 ): Promise<boolean> => {
   return await bcrypt.compare(password, hash);
+};
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export const makeJWT = (
+  userID: string,
+  expiresIn: number,
+  secret: string
+): string => {
+  const issuedAt = Math.floor(Date.now() / 1000);
+  const expiresAt = issuedAt + expiresIn;
+  const token = jwt.sign(
+    {
+      iss: TOKEN_ISSUER,
+      sub: userID,
+      iat: issuedAt,
+      exp: expiresAt,
+    } satisfies payload,
+    secret,
+    { algorithm: "HS256" }
+  );
+  return token;
+};
+
+export const validateJWT = (tokenString: string, secret: string): string => {
+  let decoded: payload;
+  try {
+    decoded = jwt.verify(tokenString, secret) as JwtPayload;
+  } catch (e) {
+    throw new UnAuthorizedError("Invalid Token");
+  }
+
+  if (decoded.iss !== TOKEN_ISSUER) {
+    throw new UnAuthorizedError("Invalid Issuer");
+  }
+
+  if (!decoded.sub) {
+    throw new UnAuthorizedError("No User ID in Token");
+  }
+
+  return decoded.sub;
 };
